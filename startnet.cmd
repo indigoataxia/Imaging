03fb55189de1a341801176fcee5577188985eab2
JP <nfg_118822@yahoo.com>
2022-05-16 15:42:10 -0400

    Update startnet.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -1,8 +1,10 @@
 wpeinit
 @echo off
 
-#find and run the imaging script, I have it on my USB under Sources\.
+#find the drive with the imaging script, copy the image script to the WinPE boot drive X, and run the imaging script. I've had best results copying the script instead of running right from USB drive incase paths somehow change
+
+#I have the script on my USB under Sources.
 
 for %%p in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do if exist %%p:sources\start.bat set w=%%p
-
-call %w%:sources\start.bat
+copy %w%:\sources\image.bat x:\image.bat
+call x:\image.bat
